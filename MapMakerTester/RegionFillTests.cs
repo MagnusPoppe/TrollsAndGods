240ca1772eddf13b471cd08ec1e27a5334e55287
MagnusPoppe <Magnus.Poppe.Wang@gmail.com>
2017-01-25 14:37:16 +0100

    Add Test Solution MapMakerTester

--- MapMakerTester/RegionFillTests.cs
+++ MapMakerTester/RegionFillTests.cs
@@ -1,19 +1,18 @@
-﻿using System;
-
+﻿using NUnit.Framework;
+using System;
 using MapGenerator;
 
-namespace MapGeneratorTests  
-{  
-
-	[TestFixture] 
-    public class RegionFillTests  
-    {
-		[Test]
-		void FloodFillTester()
+namespace MapMakerTester
+{
+	[TestFixture()]
+	public class RegionFillTests
+	{
+		[Test()]
+		public void FloodFillTester()
 		{
 			int width = 10;
 			int height = 10;
-			int[,] splitMap = new int[width,height];
+			int[,] splitMap = new int[width, height];
 
 			for (int y = 0; y < splitMap.GetLength(1); y++)
 				splitMap[5, y] = 1;
@@ -23,7 +22,7 @@ namespace MapGeneratorTests
 			{
 				for (int x = 0; x < width; x++)
 				{
-					if ( x < 5 ) expected[x, y] = 2;
+					if (x < 5) expected[x, y] = 2;
 					else if (x > 5) expected[x, y] = 3;
 				}
 			}
@@ -33,12 +32,11 @@ namespace MapGeneratorTests
 			int[,] actual = r.getMap();
 
 			Assert.AreEqual(expected, actual);
-
 		}
 
 		private int[,] copy2DArray(int[,] array)
 		{
-			int[,] copy = new int[array.GetLength(0),array.GetLength(1)];
+			int[,] copy = new int[array.GetLength(0), array.GetLength(1)];
 			for (int y = 0; y < array.GetLength(1); y++)
 			{
 				for (int x = 0; x < array.GetLength(0); x++)
@@ -48,5 +46,5 @@ namespace MapGeneratorTests
 			}
 			return copy;
 		}
-    }  
-}  
+	}
+}
