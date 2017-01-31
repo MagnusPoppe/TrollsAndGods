737d478f0f07984e6f48742f4b4a33ae673f842d
Håkon <hakomols@yahoo.no>
2017-01-31 15:41:03 +0100

    added stats package

---
+++ Assets/Scripts/Stats/HeroStats.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroStat : BaseStats
+{
+
+    int wisdom;
+    int magicPower;
+    int mana;
+    int maxMana;
+
+    public int Wisdom
+    {
+        get
+        {
+            return wisdom;
+        }
+
+        set
+        {
+            wisdom = value;
+        }
+    }
+
+    public int MagicPower
+    {
+        get
+        {
+            return magicPower;
+        }
+
+        set
+        {
+            magicPower = value;
+        }
+    }
+
+    public int Mana
+    {
+        get
+        {
+            return mana;
+        }
+
+        set
+        {
+            mana = value;
+        }
+    }
+
+    public int MaxMana
+    {
+        get
+        {
+            return maxMana;
+        }
+
+        set
+        {
+            maxMana = value;
+        }
+    }
+
+    public HeroStat(int attack, int defence, int speed, int moral, int luck,
+        int wisdom, int magicPower, int mana, int maxMana
+        ) : base(attack, defence, speed, moral, luck)
+    {
+        this.Wisdom = wisdom;
+        this.MagicPower = magicPower;
+        this.Mana = mana;
+        this.MaxMana = maxMana;
+    }
+}
---
+++ Assets/Scripts/Stats/UnitStats.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitStats : BaseStats {
+
+    int damage;
+    int health;
+    int initative;
+
+    public UnitStats(int attack, int defence, int speed, int moral, int luck,
+        int damage, int health, int initative
+        ) : base(attack, defence, speed, moral, luck)
+    {
+        this.Damage = damage;
+        this.Health = health;
+        this.Initative = initative;
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+        }
+    }
+
+    public int Initative
+    {
+        get
+        {
+            return initative;
+        }
+
+        set
+        {
+            initative = value;
+        }
+    }
+}
