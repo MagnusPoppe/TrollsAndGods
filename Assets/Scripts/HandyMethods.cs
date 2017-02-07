9b6cccef1199d77a5c0ed236ce398a3ff13941a2
Håkon <hakomols@yahoo.no>
2017-02-07 15:17:59 +0100

    updated method for getting isotilecordinate to be more dynamic

--- Assets/Scripts/HandyMethods.cs
+++ Assets/Scripts/HandyMethods.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class HandyMethods {
 
+    readonly static float HEIGHTOFISOMETRICTILE = 1;
+
 	static public Vector2 getIsoTilePos(Vector2 pos)
     {
         float x = (int)pos.x;
-        float y = (int)pos.y;
+        float y = (int)(pos.y/HEIGHTOFISOMETRICTILE)*HEIGHTOFISOMETRICTILE;
 
-        Vector2 a = new Vector2(0+x, 0.5f+y);
-        Vector2 b = new Vector2(0.5f+x, 1+y);
-        Vector2 c = new Vector2(1+x, 0.5f+y);
+        Vector2 a = new Vector2(0+x, HEIGHTOFISOMETRICTILE*0.5f+y);
+        Vector2 b = new Vector2(0.5f+x, HEIGHTOFISOMETRICTILE +y);
+        Vector2 c = new Vector2(1+x, HEIGHTOFISOMETRICTILE * 0.5f +y);
         Vector2 d = new Vector2(0.5f+x, 0+y);
 
-        if (determineSideOfLine(a, b, pos) < 0) return new Vector2(x, (y+1)*2);
-        else if (determineSideOfLine(b, c, pos) < 0) return new Vector2(x+1, (y+1)*2);
+        if (determineSideOfLine(a, b, pos) < 0) return new Vector2(x, (y+ HEIGHTOFISOMETRICTILE)*2);
+        else if (determineSideOfLine(b, c, pos) < 0) return new Vector2(x+1, (y+ HEIGHTOFISOMETRICTILE)*2);
         else if (determineSideOfLine(c, d, pos) < 0) return new Vector2(x+1, y*2);
         else if (determineSideOfLine(d, a, pos) < 0) return new Vector2(x, y*2);
-        else return new Vector2(x,(y+0.5f)*2);
+        else return new Vector2(x,(y+ HEIGHTOFISOMETRICTILE * 0.5f)*2);
     }
 
     static public float determineSideOfLine(Vector2 start, Vector2 end, Vector2 point)
