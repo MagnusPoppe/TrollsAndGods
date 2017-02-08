a0da7f7f641f5dbf7398535576216f1f310ce619
Håkon <hakomols@yahoo.no>
2017-02-08 14:52:52 +0100

    more work on Reactions

--- Assets/Scripts/OverworldObjects/ReasourceReaction.cs
+++ Assets/Scripts/OverworldObjects/ReasourceReaction.cs
@@ -9,13 +9,43 @@ public class ResourceReaction : Reaction
     int resID;
     int amount;
 
+    public int ResID
+    {
+        get
+        {
+            return resID;
+        }
+
+        set
+        {
+            resID = value;
+        }
+    }
+
+    public int Amount
+    {
+        get
+        {
+            return amount;
+        }
+
+        set
+        {
+            amount = value;
+        }
+    }
+
     public ResourceReaction(int resID, int amount, Vector2 pos, GameObject self,Reaction[,] reactionTab)
     {
-
+        ResID = resID;
+        Amount = amount;
+        Pos = pos;
+        Self = self;
+        ReactionTab = reactionTab;
     }
 
     public override bool React(Hero h)
     {
-        throw new NotImplementedException();
+        h.Player.
     }
 }
