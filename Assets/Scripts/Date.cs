e4e23c0ee753849e523ca5dbaec621861cb59b7c
Håkon <hakomols@yahoo.no>
2017-02-15 10:21:58 +0100

    made gameManager class and date class

---
+++ Assets/Scripts/Date.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Date {
+
+    int day;
+    int month;
+    int year;
+
+    public Date(int day, int month, int year)
+    {
+        Day = day;
+        Month = month;
+        Year = year;
+    }
+
+    public Date()
+    {
+        Day = 1;
+        Month = 1;
+        Year = 0;
+    }
+
+    public string incrementDay()
+    {
+        Day++;
+        if (Day > 30)
+        {
+            Day = 1;
+            Month++;
+            if (Month > 12)
+            {
+                Month = 1;
+                Year++;
+            }
+        }
+
+        return toString();
+    }
+
+    public string toString()
+    {
+        return "Day: " + Day + " Month: " + Month + "Year: " + Year;
+    }
+
+    public int Day
+    {
+        get
+        {
+            return day;
+        }
+
+        set
+        {
+            day = value;
+        }
+    }
+
+    public int Month
+    {
+        get
+        {
+            return month;
+        }
+
+        set
+        {
+            month = value;
+        }
+    }
+
+    public int Year
+    {
+        get
+        {
+            return year;
+        }
+
+        set
+        {
+            year = value;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public int amountOfPlayers;
+    Player[] players;
+    int whoseTurn;
+    Date date;
+    int[,] canWalk;
+    Reaction[,] reactions;
+
+	// Use this for initialization
+	void Start () {
+        players = new Player[amountOfPlayers];
+        whoseTurn = 0;
+        date = new Date();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0))
+        {
+            //todo add what happens when you click on something
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            // add what happens when you right click things
+        }
+	}
+
+    public Player getPlayer(int index)
+    {
+        return players[index];
+    }
+}
