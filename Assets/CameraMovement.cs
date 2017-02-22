2c428c33f5778fffe5de8c4133cec8de79e71074
Håkon <hakomols@yahoo.no>
2017-02-22 09:36:07 +0100

    fixed cameramovement to stop before showing anything outside the map

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -6,6 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     const float OFFSET = 0.5f;
     const int FRAMEOFFSET = 5;
+    const float CUTY = 0.25f;
+    const float CUTX = 0.5f;
     int width;
     int height;
     float cameraHeight;
@@ -27,21 +29,25 @@ public class CameraMovement : MonoBehaviour
 	
 	void Update ()
     {
-        if (transform.position.y < height-cameraHeight && ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) || Input.mousePosition.y > Screen.height - FRAMEOFFSET))
+        if (transform.position.y < height-cameraHeight-CUTY && ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) || Input.mousePosition.y > Screen.height - FRAMEOFFSET))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y + OFFSET, transform.position.z);
+            if (transform.position.y > height - cameraHeight - CUTY) transform.position = new Vector3(transform.position.x, height - cameraHeight - CUTY, transform.position.z);
         }
         if(transform.position.x > cameraWidth && ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) || Input.mousePosition.x < FRAMEOFFSET))
         {
             transform.position = new Vector3(transform.position.x - OFFSET, transform.position.y, transform.position.z);
+            if (transform.position.x < cameraWidth) transform.position = new Vector3(cameraWidth, transform.position.y, transform.position.z);
         }
         if (transform.position.y > cameraHeight && ((Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) || Input.mousePosition.y < FRAMEOFFSET))
         {
             transform.position = new Vector3(transform.position.x, transform.position.y - OFFSET, transform.position.z);
+            if (transform.position.y < cameraHeight) transform.position = new Vector3(transform.position.x, cameraHeight, transform.position.z);
         }
-        if (transform.position.x < width - cameraWidth && ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) || Input.mousePosition.x > Screen.width - FRAMEOFFSET))
+        if (transform.position.x < width - cameraWidth - CUTX && ((Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) || Input.mousePosition.x > Screen.width - FRAMEOFFSET))
         {
             transform.position = new Vector3(transform.position.x + OFFSET, transform.position.y, transform.position.z);
+            if (transform.position.x > width - cameraWidth -CUTX) transform.position = new Vector3(width - cameraWidth - CUTX, transform.position.y, transform.position.z);
         }
     }
 }
