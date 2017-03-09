d01ed38a2809c570f44332fb0428ae0e24b29a2f
Håkon <hakomols@yahoo.no>
2017-03-09 16:20:37 +0100

    fixed unitTree to not allow amounts to go below 0

--- Assets/Scripts/OverworldObjects/UnitTree.cs
+++ Assets/Scripts/OverworldObjects/UnitTree.cs
@@ -48,6 +48,7 @@ public class UnitTree
     public void changeAmount(int amount, int pos)
     {
         unitAmount[pos] += amount;
+        if (unitAmount[pos] < 0) unitAmount[pos] = 0;
     }
 
     /// <summary>
