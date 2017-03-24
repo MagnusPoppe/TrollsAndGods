f3b7ac25f4ab43bc9745b6b3d2ddbfa182f8a532
Håkon <hakomols@yahoo.no>
2017-03-24 15:57:17 +0100

    fixed directions to match arrays

--- Assets/Scripts/Combat/AttackDirection.cs
+++ Assets/Scripts/Combat/AttackDirection.cs
@@ -12,22 +12,22 @@ public class AttackDirection : MonoBehaviour
 	    switch (name)
 	    {
             case "NorthEast":
-	            direction = 0;
+	            direction = 1;
                 break;
             case "East":
-                direction = 1;
-                break;
-            case "SouthEast":
-                direction = 2;
-                break;
-            case "SouthWest":
                 direction = 3;
                 break;
-            case "West":
+            case "SouthEast":
+                direction = 5;
+                break;
+            case "SouthWest":
                 direction = 4;
                 break;
+            case "West":
+                direction = 2;
+                break;
             case "NorthWest":
-                direction = 5;
+                direction = 0;
                 break;
         }
 	    unitGameObject = GetComponentInParent<UnitGameObject>();
@@ -41,5 +41,6 @@ public class AttackDirection : MonoBehaviour
     private void OnMouseDown()
     {
         unitGameObject.MouseDown(direction);
+        Debug.Log(name);
     }
 }
