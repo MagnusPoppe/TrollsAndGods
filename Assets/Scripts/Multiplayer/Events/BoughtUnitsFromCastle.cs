788650ca249dd509c5ae59aaa2641c9d53e26a49
Håkon <hakomols@yahoo.no>
2017-03-29 16:01:20 +0200

    added boughtunits, switchstationed events

---
+++ Assets/Scripts/Multiplayer/Events/BoughtUnitsFromCastle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public class BoughtUnitsFromCastle : GameEvent
+    {
+        public int heroID, tier, amount;
+        public Point pos;
+
+        public BoughtUnitsFromCastle(int id, string description) : base(id, description)
+        {
+        }
+
+        public override void execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void unpackJSON(string JSON)
+        {
+            BoughtUnitsFromCastle obj = JsonUtility.FromJson<BoughtUnitsFromCastle>(JSON);
+            heroID = obj.heroID;
+            tier = obj.tier;
+            amount = obj.amount;
+            pos = obj.pos;
+        }
+
+        public override string packJSON()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    } 
+}
---
+++ Assets/Scripts/Multiplayer/Events/SwitchStationedHero.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Multiplayer
+{
+    public class SwitchStationedHero : GameEvent
+    {
+        private Point pos;
+        private bool stationed;
+
+        public SwitchStationedHero(int id, string description, Point pos, bool stationed) : base(id, description)
+        {
+            this.pos = pos;
+            this.stationed = stationed;
+        }
+
+        public override void execute()
+        {
+            //todo flip stationed hero in castle
+            throw new System.NotImplementedException();
+        }
+
+        public override void unpackJSON(string JSON)
+        {
+            SwitchStationedHero obj = JsonUtility.FromJson<SwitchStationedHero>(JSON);
+            pos = obj.pos;
+            stationed = obj.stationed;
+        }
+
+        public override string packJSON()
+        {
+            return JsonUtility.ToJson(this);
+        }
+    } 
+}
